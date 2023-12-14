0b33dc73adab683700784d8a0448c1da9d7e67c9
Rocco Spadone <rocco4064@gmail.com>
2023-12-15 00:53:01 +0100

    added deezer api wrapper (search)

---
+++ Deezer/Deezer.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using RestSharp;
+
+namespace NowPlaySharpBot.Deezer;
+
+public static class Deezer
+{
+    
+    private const string Resource = "https://api.deezer.com";
+
+    private static readonly RestClientOptions Options = new RestClientOptions(Resource) {
+        ThrowOnAnyError = false,
+        ThrowOnDeserializationError = false
+    };
+    private static readonly RestClient Client = new RestClient(options: Options);
+    
+    public static async Task<TrackResponse> Search(string query)
+    {
+        var request = new RestRequest("search/track", Method.Post);
+        request.AddParameter("q", query);
+        var response = await Client.ExecuteGetAsync(request);
+        return JsonSerializer.Deserialize<TrackResponse>(response.Content ?? throw new MissingFieldException()) ?? throw new Exception("wtf!?");;
+    }
+}
---
+++ Deezer/TrackResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NowPlaySharpBot;
+
+public record TrackResponse
+{
+    [JsonPropertyName("data")] public List<Data>? Data { get; init; }
+}
+
+public record Data
+{
+    [JsonPropertyName("id")] public long Id { get; init; }
+    [JsonPropertyName("title")] public string Title { get; init; }
+    [JsonPropertyName("artist")] public Artist Artist { get; init; }
+    [JsonPropertyName("album")] public Album Album { get; init; }
+    [JsonPropertyName("duration")] public int Duration { get; init; }
+    [JsonPropertyName("preview")] public string Preview { get; init; }
+}
+
+public record Artist
+{
+    [JsonPropertyName("name")] public string Name { get; init; }
+}
+
+public record Album
+{
+    [JsonPropertyName("cover_big")] public string ImgUrl { get; init; }
+}
